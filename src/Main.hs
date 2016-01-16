ad830068ded7943cda7ff925de4e6fff633fad76
SeTSeR <sergey200018@bk.ru>
2016-01-17 01:50:05 +0300

    Added another histogram, which is not working now

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,32 @@
 module Main where
 
+import Data.Vector as V         (replicate, (//), (!), length, head, tail)
 import Control.Monad.IO.Class
 import Random
 import Graphics.UI.Gtk          hiding (fill, rectangle)
 import Graphics.UI.Gtk.Builder
 import Graphics.Rendering.Cairo
 
-drawRect :: Double -> Double -> Double -> Double -> Render ()
-drawRect x y w h = do
-    setSourceRGB 0 1 0
-    rectangle x y w h
-    fill
+drawRHistogram :: Int -> Int -> ((Int, Int) -> (Int, Int)) -> (Int, Int) -> Int -> Render ()
+drawRHistogram w h f (x1, x2) m = setSourceRGB 1 1 1 >> rectangle 0 0 w' h' >> fill >> go 1 (go1 f (x1, x2) 400 m (V.replicate 400 0))
+    where
+        h'  = fromIntegral h
+        w'  = fromIntegral w
+        w'' = w'/20
+        m'  = fromIntegral m
+        myheight x = (fromIntegral x)*h'/m'
+        get x1 m = (x1*20) `div` m
+        go1 f (x1, x2) n m arr | n==1      = arr
+                               | n==400    = go1 f (f (x1, x2)) (n-1) m (arr // [((get x1 m), arr!(get x1 m))])
+                               | otherwise = go1 f (f (x1, x2)) (n-1) m (arr // [((get x2 m), arr!(get x2 m))])
+        drawRect x y w h = do
+            setSourceRGB 0 0 1
+            rectangle x y w h
+            fill
+        go x arr | (V.length arr) == 0 = return ()
+                 | otherwise           = do
+                drawRect ((fromIntegral (x-1))*w'') (h' - myheight (V.head arr)) w'' (myheight (V.head arr))
+                go (x+1) (V.tail arr)
 
 drawHistogram :: Int -> Int -> ((Int, Int) -> (Int, Int)) -> (Int, Int) -> Int -> Int -> Render ()
 drawHistogram w h f (x1, x2) n m = setSourceRGB 1 1 1 >> rectangle 0 0 w' h' >> fill >> go f (x1, x2) 1 n
@@ -18,8 +34,12 @@ drawHistogram w h f (x1, x2) n m = setSourceRGB 1 1 1 >> rectangle 0 0 w' h' >> 
         w'  = fromIntegral w
         h'  = fromIntegral h
         w'' = w'/(fromIntegral n)
-        m' = fromIntegral m
+        m'  = fromIntegral m
         myheight x = (fromIntegral x)*h'/m'
+        drawRect x y w h = do
+            setSourceRGB 0 1 0
+            rectangle x y w h
+            fill
         go f (x1, x2) m n | m==1      = do
             drawRect ((fromIntegral (m-1))*w'') (h' - myheight x1) w'' (myheight x1)
             go f (f (x1, x2)) (m+1) n
@@ -38,12 +58,18 @@ recalc builder = do
             return $ read text
         labels = ["a", "b", "c", "m", "x1", "x2"]
     [a, b, c, m, x1, x2] <- mapM parseText ["entry" ++ l | l <- labels]
-    area    <- builderGetObject builder castToDrawingArea "drawingarea1"
-    w       <- widgetGetAllocatedWidth area
-    h       <- widgetGetAllocatedHeight area
+    let getAreas label = do
+            area <- builderGetObject builder castToDrawingArea label
+            w    <- widgetGetAllocatedWidth area
+            h    <- widgetGetAllocatedHeight area
+            return (area, w, h)
+        labels = ["1", "2"]
+    [(area1, w1, h1), (area2, w2, h2)] <- mapM getAreas ["drawingarea" ++ l | l <- labels]
+    area1 `on` draw $ (drawHistogram w1 h1 (gen a b c m) (x1, x2) 100 m)
+    area2 `on` draw $ (drawRHistogram w2 h2 (gen a b c m) (x1, x2) m)
     putStrLn $ research (gen a b c m) (x1, x2) m
-    area `on` draw $ (drawHistogram w h (gen a b c m) (x1, x2) 100 m)
-    widgetQueueDraw area
+    widgetQueueDraw area1
+    widgetQueueDraw area2
 
 main :: IO ()
 main = do
